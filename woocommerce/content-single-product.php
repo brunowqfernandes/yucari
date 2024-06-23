55b2f8f427ed2fe2846d62b971fd76e1c47d8f9f
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-06-23 11:28:59 -0300

    a

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ if (post_password_required())
    return;
 }
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class('flex', $product); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
 
    <?php
    /**
@@ -45,7 +45,7 @@ if (post_password_required())
    do_action('woocommerce_before_single_product_summary');
    ?>
 
-   <div class="summary entry-summary w-[40%]">
+   <div class="summary entry-summary">
       <?php
       /**
        * Hook: woocommerce_single_product_summary.
