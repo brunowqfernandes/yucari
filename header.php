c22bf180f7aa58aa19f9bf64e5057d690420deec
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-06-29 22:47:51 -0300

    a

--- header.php
+++ header.php
@@ -97,7 +97,7 @@ $class_count = $numero_produtos_carrinho <= 99 ? 'text-xs' : 'text-[0.625rem]';
             </div>
          </div>
       </div>
-      <div :class="{'-mr-0': menuMobile === true, '-mr-[100vw]': menuMobile === false}" class="menu-mobile lg:hidden w-full h-full fixed -mr-[100vw] bg-purple-900 text-white top-0 right-0 transition-all animate-duration-300 z-[999]">
+      <div :class="{'-mr-0': menuMobile === true, '-mr-[100vw]': menuMobile === false}" class="menu-mobile lg:hidden w-full h-full fixed -mr-[100vw] bg-purple-900 bg-opacity-90 text-white top-0 right-0 transition-all animate-duration-300 z-[999]">
          <button @click="menuMobile = false" class="absolute top-0 right-0 m-4 p-2 text-white rounded-full">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
