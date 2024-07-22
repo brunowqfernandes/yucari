ff314968c49718fb89cb33290c09c649b68cd381
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-07-22 17:56:49 -0300

    a

--- functions.php
+++ functions.php
@@ -119,6 +119,15 @@ function yucari_content_width()
 }
 add_action('after_setup_theme', 'yucari_content_width', 0);
 
+function move_coupon_form() {
+   // Remove o formulário de cupom da posição atual
+   remove_action('woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10);
+   
+   // Adiciona o formulário de cupom após os campos de checkout
+   add_action('woocommerce_after_checkout_form', 'woocommerce_checkout_coupon_form', 10);
+}
+add_action('wp_loaded', 'move_coupon_form');
+
 /**
  * Register widget area.
  *
